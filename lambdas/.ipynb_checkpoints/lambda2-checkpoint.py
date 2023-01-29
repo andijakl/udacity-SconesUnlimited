2d5382cd6cd17d47c1c5f4fdc78d15ada699ba96
Andreas Jakl <andreas.jakl@live.com>
2023-01-29 16:39:19 +0000

    add further files

---
+++ lambdas/.ipynb_checkpoints/lambda2-checkpoint.py
@@ -0,0 +1,28 @@
+import json
+import sagemaker
+import base64
+from sagemaker.serializers import IdentitySerializer
+
+# Fill this in with the name of your deployed model
+ENDPOINT = 'image-classification-2023-01-29-12-43-28-203' ## TODO: fill in
+
+def lambda_handler(event, context):
+
+    # Decode the image data
+    image = base64.b64decode(event['body']['image_data'])
+
+    # Instantiate a Predictor
+    predictor = sagemaker.predictor.Predictor(ENDPOINT) ## TODO: fill in
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+    
+    # Make a prediction:
+    inferences = predictor.predict(image) ## TODO: fill in
+    
+    # We return the data back to the Step Function    
+    event["body"]["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': event["body"]
+    }
---
+++ lambdas/lambda3.py
@@ -0,0 +1,31 @@
+import json
+
+
+THRESHOLD = .93
+
+
+def lambda_handler(event, context):
+    
+    # Grab the inferences from the event
+    inferences = event['body']['inferences'] ## TODO: fill in
+    # Make sure we have a valid Python dictionary containing floats and not a string
+    inferences = json.loads(inferences)
+    
+    # Some debug code
+    print(inferences)
+    print(type(inferences[0]))
+    
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold = any([x > THRESHOLD for x in inferences]) ## TODO: fill in
+    
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': event["body"]
+    }
